f480e8642b43989e95e8a8e399661b38e70196b5
rzimmerdev <rzimmerdev@gmail.com>
2022-05-22 02:05:57 -0300

    [ w2v_train.py ] - Working on DataLoader and Training Loop

---
+++ src/w2v_train.py
@@ -0,0 +1,177 @@
+import json
+import os
+
+import numpy as np
+from torch import nn, no_grad, save
+from torch.utils.data import Dataset
+
+SKIPGRAM_N_WORDS = 4
+
+MIN_WORD_FREQUENCY = 50
+MAX_SEQUENCE_LENGTH = 256
+
+EMBED_DIMENSION = 300
+EMBED_MAX_NORM = 1
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, img_labels, img_dir, transform=None, target_transform=None):
+        self.img_labels = img_labels
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        # img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        # image = read_image(img_path)
+        # label = self.img_labels.iloc[idx, 1]
+        # if self.transform:
+        #     image = self.transform(image)
+        # if self.target_transform:
+        #     label = self.target_transform(label)
+        # return image, label
+        pass
+
+
+def get_input_layer(word_idx):
+    class SkipGram(nn.Module):
+        """
+        Implementation of Skip-Gram model described in paper:
+        https://arxiv.org/abs/1301.3781
+        """
+
+        def __init__(self, vocab_size: int):
+            super(SkipGram, self).__init__()
+            self.embeddings = nn.Embedding(
+                num_embeddings=vocab_size,
+                embedding_dim=EMBED_DIMENSION,
+                max_norm=EMBED_MAX_NORM,
+            )
+            self.linear = nn.Linear(
+                in_features=EMBED_DIMENSION,
+                out_features=vocab_size,
+            )
+
+        def forward(self, inputs_):
+            x = self.embeddings(inputs_)
+            x = self.linear(x)
+            return x
+
+
+class TrainLoop:
+    """Main class for model training"""
+
+    def __init__(
+            self,
+            model,
+            epochs,
+            train_dataloader,
+            train_steps,
+            val_dataloader,
+            val_steps,
+            checkpoint_frequency,
+            criterion,
+            optimizer,
+            lr_scheduler,
+            device,
+            model_dir,
+            model_name,
+    ):
+        self.model = model
+        self.epochs = epochs
+        self.train_dataloader = train_dataloader
+        self.train_steps = train_steps
+        self.val_dataloader = val_dataloader
+        self.val_steps = val_steps
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.checkpoint_frequency = checkpoint_frequency
+        self.lr_scheduler = lr_scheduler
+        self.device = device
+        self.model_dir = model_dir
+        self.model_name = model_name
+
+        self.loss = {"train": [], "val": []}
+        self.model.to(self.device)
+
+    def train(self):
+        for epoch in range(self.epochs):
+            self._train_epoch()
+            self._validate_epoch()
+            print(
+                "Epoch: {}/{}, Train Loss={:.5f}, Val Loss={:.5f}".format(
+                    epoch + 1,
+                    self.epochs,
+                    self.loss["train"][-1],
+                    self.loss["val"][-1],
+                )
+            )
+
+            self.lr_scheduler.step()
+
+            if self.checkpoint_frequency:
+                self._save_checkpoint(epoch)
+
+    def _train_epoch(self):
+        self.model.train()
+        running_loss = []
+
+        for i, batch_data in enumerate(self.train_dataloader, 1):
+            inputs = batch_data[0].to(self.device)
+            labels = batch_data[1].to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self.model(inputs)
+            loss = self.criterion(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+            running_loss.append(loss.item())
+
+            if i == self.train_steps:
+                break
+
+        epoch_loss = np.mean(running_loss)
+        self.loss["train"].append(epoch_loss)
+
+    def _validate_epoch(self):
+        self.model.eval()
+        running_loss = []
+
+        with no_grad():
+            for i, batch_data in enumerate(self.val_dataloader, 1):
+                inputs = batch_data[0].to(self.device)
+                labels = batch_data[1].to(self.device)
+
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+
+                running_loss.append(loss.item())
+
+                if i == self.val_steps:
+                    break
+
+        epoch_loss = np.mean(running_loss)
+        self.loss["val"].append(epoch_loss)
+
+    def _save_checkpoint(self, epoch):
+        """Save model checkpoint to `self.model_dir` directory"""
+        epoch_num = epoch + 1
+        if epoch_num % self.checkpoint_frequency == 0:
+            model_path = "checkpoint_{}.pt".format(str(epoch_num).zfill(3))
+            model_path = os.path.join(self.model_dir, model_path)
+            save(self.model, model_path)
+
+    def save_model(self):
+        """Save final model to `self.model_dir` directory"""
+        model_path = os.path.join(self.model_dir, "model.pt")
+        save(self.model, model_path)
+
+    def save_loss(self):
+        """Save train/val loss as json file to `self.model_dir` directory"""
+        loss_path = os.path.join(self.model_dir, "loss.json")
+        with open(loss_path, "w") as fp:
+            json.dump(self.loss, fp)
